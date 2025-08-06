73ad19d7cb8e7d32cc4bd4562cdd8f5d5d6afa92
almazova09 <almazova1509@gmail.com>
2025-08-06 23:20:38 +0000

    changes

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource aws_instance web {
     ami           = "ami-08ca1d1e465fbfe0c"
     instance_type = "t2.micro"
 }
+
+terraform {
+    backend "s3" {
+        bucket = "kaizen-shirina"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+    
+}
